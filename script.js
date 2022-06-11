78ef5b33cb95ef64acb3d0ad865caf4a3a74ea03
ivanf-12 <febriantoivan12@gmail.com>
2022-06-11 17:56:54 +0700

    add js password and phone number validator

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-const submitButton = document.querySelector('button');
-const passwordField = document.querySelector('#password');
-const confirmPwField = document.querySelector('#confirm-pw');
+const submitButton = document.querySelector("[data-submit]");
+const passwordField = document.querySelector('[data-password]');
+const confirmPwField = document.querySelector('[data-confirmPw]');
+const numberField = document.querySelector('[data-number]');
 
 submitButton.addEventListener('click', () => {
-  console.log(passwordField);
+  console.log(numberField);
+  if(passwordField.value !== confirmPwField.value) {
+    confirmPwField.setCustomValidity("password doesn't match!");
+  }
+  else {
+    confirmPwField.setCustomValidity("");
+  }
+
+  if(parseInt(numberField.value) === NaN) {
+    numberField.setCustomValidity("Please enter 10 digit telephone numbers!");
+  }
+  else {
+    if(numberField.value.length != 10) {
+      numberField.setCustomValidity("Please enter 10 digit telephone numbers!");
+    }
+    else {
+      numberField.setCustomValidity("");
+    }
+  }
 });
