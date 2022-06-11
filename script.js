f167c31cfb1beb9ed5596743b4c1f305827b1d15
ivanf-12 <febriantoivan12@gmail.com>
2022-06-11 17:19:12 +0700

    adjust input field width for big laptops

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("halo");
+const submitButton = document.querySelector('button');
+const passwordField = document.querySelector('#password');
+const confirmPwField = document.querySelector('#confirm-pw');
+
+submitButton.addEventListener('click', () => {
+  console.log(passwordField);
+});
