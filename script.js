5386a3fccd10979bdb149a28fc0436c7a2a63c06
ivanf-12 <febriantoivan12@gmail.com>
2022-06-12 17:30:45 +0700

    fix number validator

--- script.js
+++ script.js
@@ -4,24 +4,39 @@ const confirmPwField = document.querySelector('[data-confirmPw]');
 const numberField = document.querySelector('[data-number]');
 
 submitButton.addEventListener('click', () => {
-  console.log(numberField);
-  if(passwordField.value !== confirmPwField.value) {
+  for(let i=0; i<numberField.value.length; ++i) {
+    let chr = numberField.value[i];
+    if(isNaN(chr)) {
+      console.log("empat")
+      numberField.setCustomValidity("Please enter 10 digits telephone number!");
+      return;
+    }
+  }
+  numberField.setCustomValidity("");
+  if(numberField.value.length !== 10) {
+    console.log("lima")
+    numberField.setCustomValidity("Please enter 10 digits telephone number!");
+    return;
+  }
+  else {
+    console.log("enam")
+    numberField.setCustomValidity("");
+  }
+
+  if(passwordField.value.length === 0) {
+    console.log("satu")
+    passwordField.setCustomValidity("password can't be empty!");
+    return;
+  }
+  else if(passwordField.value !== confirmPwField.value) {
+    console.log("dua")
+    passwordField.setCustomValidity("");
     confirmPwField.setCustomValidity("password doesn't match!");
     return;
   }
   else {
+    console.log("tiga")
+    passwordField.setCustomValidity("");
     confirmPwField.setCustomValidity("");
-  }
-
-  if(parseInt(numberField.value) === NaN) {
-    numberField.setCustomValidity("Please enter 10 digit telephone numbers!");
-  }
-  else {
-    if(numberField.value.length != 10) {
-      numberField.setCustomValidity("Please enter 10 digit telephone numbers!");
-    }
-    else {
-      numberField.setCustomValidity("");
-    }
   }
 });
